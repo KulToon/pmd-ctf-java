de43361014a290dbbda62e4fb4591826e310dce5
Lashang166 <bankteeradhol@gmail.com>
2021-05-13 12:58:53 +0700

    first commit

---
+++ src/com/buzzfreeze/helloworld/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.buzzfreeze.helloworld;
+
+public class HelloWorld {
+	public static void main (String [] args) {
+		
+		int number = 19;
+		String str = "Hello world";
+		float pi = 3.14f;
+		char c = 'c';
+		boolean t = true;
+		int[] myNum = {10, 20 ,30};
+		
+	
+		System.out.println("This is int " + number);
+		System.out.println("This is String " + str);
+		System.out.println("This is float " + pi);
+		System.out.println("This is boolean " + t);
+		System.out.println("This is char " + c);
+		
+//		System.out.println("This is char " + c);
+		/*System.out.println("This is char " + c);
+		System.out.println("This is char " + c);
+		System.out.println("This is char " + c);*/
+
+	}
+}
